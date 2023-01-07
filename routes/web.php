c35ecc9feb7cbba821050e3c514ffe201a9203ef
xcl21 <axcle.viray21@gmail.com>
2023-01-07 13:30:18 +0800

    route for user complete (for further edition)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 
@@ -49,10 +52,49 @@ Route::prefix('admin')->group(function () {
     });
 });
 
+/**
+ * ==================== USER ROUTE ====================
+ */
+Route::controller(UserController::class)->group(function () {
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/dashboard', 'index')->name('user.dashboard');
+        Route::get('/logout', 'logout')->name('user.logout');
+        Route::get('/profile/edit', 'edit_profile')->name('user.edit_profile');
+        Route::get('/password/edit', 'edit_password')->name('user.edit_password');
+
+        Route::put('/profile/update', 'update_profile')->name('user.update_profile');
+        Route::put('/password/update', 'update_password')->name('user.update_password');
+    });
+    Route::get('/register', 'create')->name('user.register_form');
+    Route::get('/login', 'login_form')->name('user.login_form');
+
+    Route::post('/login', 'login')->name('user.login');
+});
+
+Route::controller(NoteController::class)->group(function () {
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/note', 'index')->name('note.index');
+        Route::get('/note/create', 'create')->name('note.create');
+        Route::get('/note/trash', 'trash')->name('note.trash');
+        Route::get('/note/archives', 'archives')->name('note.archives');
+        Route::get('/note/{id}', 'destroy')->name('note.destroy');
+        Route::get('/note/show/{id}', 'show')->name('note.show');
+        Route::get('/note/edit/{id}', 'edit')->name('note.edit');
+        Route::get('/note/archive/{id}', 'archive')->name('note.archive');
+        Route::get('/note/delete/{id}', 'delete')->name('note.delete');
+        Route::get('/note/restore/{id}', 'restore')->name('note.restore');
+        Route::get('/note/unarchive/{id}', 'unarchive')->name('note.unarchive');
+
+        Route::post('/note/store', 'store')->name('note.store');
+        Route::put('/note/{id}', 'update')->name('note.update');
+    });
+});
+
 Route::controller(TagController::class)->group(function () {
     Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('/tag', 'index')->name('tag.index');
-        Route::get('/tag/create', 'create')->name('tag.create');
+        // Route::get('/tag/create', 'create')->name('tag.create');
+        Route::get('/tag/edit/{id}', 'edit')->name('tag.edit');
         Route::get('/tag/show/{id}', 'show')->name('tag.show');
         Route::get('/tag/{id}', 'destroy')->name('tag.destroy');
 
