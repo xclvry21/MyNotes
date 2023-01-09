926cb465a8f59f6d586b8658494fa2cf9c81fd8a
xcl21 <axcle.viray21@gmail.com>
2023-01-09 20:31:53 +0800

    remove some links on masters

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -8,17 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />
+        
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('dashboard_body') }}/assets/images/mynotes_favicon.ico">
-        
-        <!-- jquery.vectormap css -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
-
-        <!-- DataTables -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-
-        <!-- Responsive datatable examples -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />  
 
         <!-- Bootstrap Css -->
         <link href="{{ asset('dashboard_body') }}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
@@ -31,11 +23,17 @@
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-select-bs4/css//select.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <!-- Responsive datatable examples -->
+        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />  
 
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css" >
-
+        {{-- toastr --}}
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
-        
+
+        {{-- Select2 (multi select dropdown) --}}
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+        {{-- summernote --}}
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 
     </head>
 
@@ -88,28 +86,35 @@
         <script src="{{ asset('dashboard_body') }}/assets/libs/metismenu/metisMenu.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/simplebar/simplebar.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/node-waves/waves.min.js"></script>
-
-        
-        <!-- apexcharts -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/apexcharts/apexcharts.min.js"></script>
-
-        <!-- jquery.vectormap map -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
-
+ 
+        <!-- Datatable init js -->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/datatables.init.js"></script>
         <!-- Required datatable js -->
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-        
         <!-- Responsive examples -->
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
+         
         <script src="{{ asset('dashboard_body') }}/assets/js/pages/dashboard.init.js"></script>
-
+ 
+        <!-- init js -->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/form-editor.init.js"></script>
+ 
+        {{-- sweetalert --}}
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+ 
+        <script src="{{ asset('dashboard_body') }}/assets/js/code.js"></script>
+ 
+        <!-- materialdesign icon js-->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/materialdesign.init.js"></script>
+ 
         <!-- App js -->
         <script src="{{ asset('dashboard_body') }}/assets/js/app.js"></script>
-
+        
+        {{-- moment --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/moment.min.js"></script>
+        
         {{-- Toastr --}}
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
         <script>
@@ -135,44 +140,6 @@
         @endif 
         </script>
 
-        <!--tinymce js-->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/tinymce/tinymce.min.js"></script>
-
-        <!-- init js -->
-        <script src="{{ asset('dashboard_body') }}/assets/js/pages/form-editor.init.js"></script>
-
-        <!-- Required datatable js -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-        <!-- Buttons examples -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/jszip/jszip.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/pdfmake/build/pdfmake.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/pdfmake/build/vfs_fonts.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
-
-        <!-- Responsive examples -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
-        <!-- Datatable init js -->
-        <script src="{{ asset('dashboard_body') }}/assets/js/pages/datatables.init.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-        <script src="{{ asset('dashboard_body') }}/assets/js/code.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js" ></script>
-        
-        {{-- moment --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/moment.min.js"></script>
-        
         {{-- admin modal --}}
         <script type='text/javascript'>
             $(document).ready(function () {
--- resources/views/user/user_master.blade.php
+++ resources/views/user/user_master.blade.php
@@ -8,17 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose user & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />
+
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('dashboard_body') }}/assets/images/mynotes_favicon.ico">
-        
-        <!-- jquery.vectormap css -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/user-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
-
-        <!-- DataTables -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-
-        <!-- Responsive datatable examples -->
-        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />  
 
         <!-- Bootstrap Css -->
         <link href="{{ asset('dashboard_body') }}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
@@ -31,9 +23,10 @@
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
         <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-select-bs4/css//select.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <!-- Responsive datatable examples -->
+        <link href="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />  
 
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css" >
-
+        {{-- toastr --}}
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
         {{-- Select2 (multi select dropdown) --}}
@@ -94,20 +87,30 @@
         <script src="{{ asset('dashboard_body') }}/assets/libs/simplebar/simplebar.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/node-waves/waves.min.js"></script>
 
-        <!-- jquery.vectormap map -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/user-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/user-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
-
+        <!-- Datatable init js -->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/datatables.init.js"></script>
         <!-- Required datatable js -->
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-        
         <!-- Responsive examples -->
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
         <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
+        
         <script src="{{ asset('dashboard_body') }}/assets/js/pages/dashboard.init.js"></script>
 
+        <!-- init js -->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/form-editor.init.js"></script>
+
+        {{-- sweetalert --}}
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+        <script src="{{ asset('dashboard_body') }}/assets/js/code.js"></script>
+
+        <!-- materialdesign icon js-->
+        <script src="{{ asset('dashboard_body') }}/assets/js/pages/materialdesign.init.js"></script>
+
+        <!-- App js -->
+        <script src="{{ asset('dashboard_body') }}/assets/js/app.js"></script>
     
         {{-- Toastr --}}
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
@@ -154,41 +157,6 @@
             });
         </script>
 
-        <!-- init js -->
-        <script src="{{ asset('dashboard_body') }}/assets/js/pages/form-editor.init.js"></script>
-
-        <!-- Required datatable js -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-        <!-- Buttons examples -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/jszip/jszip.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/pdfmake/build/pdfmake.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/pdfmake/build/vfs_fonts.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
-
-        <!-- Responsive examples -->
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-        <script src="{{ asset('dashboard_body') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
-        <!-- Datatable init js -->
-        <script src="{{ asset('dashboard_body') }}/assets/js/pages/datatables.init.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-        <script src="{{ asset('dashboard_body') }}/assets/js/code.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js" ></script>
-
-        <!-- materialdesign icon js-->
-        <script src="{{ asset('dashboard_body') }}/assets/js/pages/materialdesign.init.js"></script>
-        
         {{-- select2 --}}
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
         <script>
@@ -225,9 +193,6 @@
                 $('#datatable-trash').DataTable();
             });
         </script>
-
-        <!-- App js -->
-        <script src="{{ asset('dashboard_body') }}/assets/js/app.js"></script>
 
     </body>
 
