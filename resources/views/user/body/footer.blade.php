b833e1d46c41f75158f14d19458d6093ab54306f
xcl21 <axcle.viray21@gmail.com>
2023-05-25 21:34:21 +0800

    update footer

--- resources/views/user/body/footer.blade.php
+++ resources/views/user/body/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> {{config('app.name')}} © Upcube.
+                <script>document.write(new Date().getFullYear())</script> {{config('app.name')}} © Axcle Viray.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by {{config('app.name')}}
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://elements.envato.com/upcube-bootstrap-admin-dashboard-template-QEGQA8T" target="_blank">Upcube</a>
                 </div>
             </div>
         </div>
